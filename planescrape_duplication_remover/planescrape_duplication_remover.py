070ecec79c8702c52a8404b600e971d48449286a
tyjeon <taeyongjeon24@gmail.com>
2020-11-06 00:29:36 +0900

    Update planescrape_duplication_remover.py

--- planescrape_duplication_remover/planescrape_duplication_remover.py
+++ planescrape_duplication_remover/planescrape_duplication_remover.py
@@ -1,24 +1,28 @@
 # This file is used to delete the repeated dialogues of the game Planescape: Torment: Enhanced Edition.
 
 import re
+from pprint import pprint
 
+FILENAME = "planescape_dialogue.txt"
+MODIFIED_FILENAME = "planescape_dialogue_without_duplication.txt"
 NUMBER_REGEX = r"^\d+:$"
 
-def elements_are_not_blank(temp):
+def are_elements_not_blank(temp):
     for temp_index, temp_text in enumerate(temp):
         if temp_index != 0 and temp_text != "":
             return True
     return False
 
-def it_is_last_element():
+def is_last_element():
     return index == len(lines) - 1
 
-def it_is_double_line_break(index,texts):
+def is_double_line_break(index,texts):
     return index > 0 and texts[index - 1] == "" and texts[index] == ""
 
 def main():
-    with open("planescape_dialogue.txt", encoding="utf-8", mode="r") as f:
+    with open(FILENAME, encoding="utf-8", mode="r") as f:
         lines = f.readlines()
+
 
 
     temp = []
@@ -27,16 +31,17 @@ def main():
     for index, line in enumerate(lines):
         try:
             number_text = re.compile(NUMBER_REGEX).search(line).group()
+            pprint(number_text)
 
-            if len(temp) == 0:
+            if len(temp) == 0: # first line of the file.
                 temp.append(number_text)
 
             else:
-                if elements_are_not_blank(temp):
+                if are_elements_not_blank(temp):
                     for temp_text in temp:
                         texts.append(temp_text.strip())
 
-                elif it_is_last_element(index,lines):
+                elif is_last_element(index,lines):
                     for temp_text in temp:
                         texts.append(temp_text.strip())
 
@@ -54,10 +59,10 @@ def main():
                 temp.append(line.strip())
 
 
-    with open("planescape_dialogue_without_duplication.txt", encoding="utf-8", mode="w") as f:
+    with open(MODIFIED_FILENAME, encoding="utf-8", mode="w") as f:
         for index, line in enumerate(texts):
 
-            if it_is_double_line_break(index,texts):
+            if is_double_line_break(index,texts):
                 continue
 
         else:
